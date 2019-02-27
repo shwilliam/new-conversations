8e1634a55edfcb3d6180381009c6742abadbc632
William L <w-lindvall@outlook.com>
2019-02-27 14:58:52 -0500

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'spa',
 
   head: {
-    title: pkg.name,
+    title: 'new conversations',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
